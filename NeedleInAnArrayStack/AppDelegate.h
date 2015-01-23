eeffb1a00129023c7a05c4978ce1ca2d23f7bbe6
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-01-23 16:24:08 -0500

    adds base project

---
+++ NeedleInAnArrayStack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeedleInAnArrayStack
+//
+//  Created by Chris Gonzales on 1/23/15.
+//  Copyright (c) 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
